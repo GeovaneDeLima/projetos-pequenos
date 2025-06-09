25aada1cfb1c154449b8e4cf43743b018234c299
Geovane Lima <geovane020250@gmail.com>
2025-06-09 11:35:00 -0300

    ,

--- projeto 004/script.js
+++ projeto 004/script.js
@@ -1,8 +1,32 @@
-const icon = document.getElementById('icon')
-const exc = document.getElementById('exc') 
+const text = document.getElementById('add') 
+const butadd = document.getElementById('butadd')
+let tarefa = []
 
-icon.addEventListener('click', aparecer)
 
-function aparecer(){
-    exc.style.display = 'block'
+butadd.addEventListener('click', adicionar)
+
+
+function adicionar (){
+    tarefa.push(text.value)
+    let ultimat = tarefa.at(-1)
+    
+    const article = document.getElementById('article')
+    const div = document.createElement('div')
+    const nome = document.createElement('p')
+    const but = document.createElement('span')
+    
+    div.classList.add('tarefa')
+    nome.classList.add('nome')
+    but.classList.add('material-symbols-outlined')
+
+    article.appendChild(div)
+    div.appendChild(nome)
+    div.appendChild(but)
+
+    nome.innerHTML = ultimat
+    but.innerHTML = 'delete'
+    
+    nome.addEventListener('click', () => {nome.style.textDecoration = 'line-through'})
+    
+    but.addEventListener('click', () => {div.remove()})
 }
